a115de26b98368abc05f447807e59ca67d0613a7
Daan van Berkel <daan.van.berkel@alliander.com>
2020-01-21 19:37:56 +0100

    create a prelude

---
+++ examples/tower.rs
@@ -0,0 +1,16 @@
+extern crate camel_up;
+
+use camel_up::prelude::*;
+
+fn main() {
+    let race = "gyor,,,w".parse::<Race>().expect("to parse");
+    let dice = "gyorw".parse::<Dice>().expect("to parse");
+
+    let result = project(&race, &dice);
+    let mut ordered: Vec<(Camel, Fraction)> = result.iter().map(|(k, v)| (*k, *v)).collect();
+    ordered.sort_by(|(_, left), (_, right)| right.cmp(&left));
+    for (camel, fraction) in ordered {
+        print!("({:?},{})", camel, fraction);
+    }
+    println!()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,11 @@ pub mod fraction;
 #[deny(missing_docs)]
 pub mod oracle;
 mod tree;
+
+pub mod prelude {
+    //! Easy access to a good combination of camel up related concepts.
+
+    pub use crate::camel::{Camel, Dice, Race};
+    pub use crate::fraction::Fraction;
+    pub use crate::oracle::project;
+}
